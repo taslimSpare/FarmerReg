51873f54a205beca3febd25921c8b0c1e12ac96c
Taslim Oseni <tasleemoseni@gmail.com>
2020-05-23 18:52:23 +0100

    Room database initialized

---
+++ app/src/main/java/com/amazon/app/farmerreg/db/MyAppDatabase.kt
@@ -0,0 +1,33 @@
+package com.amazon.app.farmerreg.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.amazon.app.farmerreg.R
+import com.amazon.app.farmerreg.pojo.UserProfile
+
+@Database(version = 1, entities = [UserProfile::class], exportSchema = false)
+abstract class MyAppDatabase : RoomDatabase() {
+
+    abstract fun userProfileDao(): UserProfileDao
+
+    companion object {
+        private var sInstance: MyAppDatabase? = null
+
+        fun getAppDatabase(context: Context): MyAppDatabase? {
+            if (sInstance == null) {
+                synchronized(MyAppDatabase::class) {
+                    sInstance = buildDatabase(context)
+                }
+            }
+            return sInstance
+        }
+
+        private fun buildDatabase(context: Context): MyAppDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                MyAppDatabase::class.java, context.getString(R.string.app_name)).build()
+        }
+    }
+}
---
+++ app/src/main/java/com/amazon/app/farmerreg/pojo/UserProfile.kt
@@ -0,0 +1,10 @@
+package com.amazon.app.farmerreg.pojo
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "profile")
+data class UserProfile(@PrimaryKey var id: Int = 0, var name: String = "", var email: String = "", var phone: String = "") {
+
+    constructor() : this(0)
+}
