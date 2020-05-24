5f5ab7e08f904c9cd82f960b47e52b763af7a339
Taslim Oseni <tasleemoseni@gmail.com>
2020-05-24 02:47:47 +0100

    Created UI mocks for home, profile and allFarms fragment

---
+++ app/src/main/java/com/amazon/app/farmerreg/view/fragments/AllFarmersFragment.kt
@@ -0,0 +1,26 @@
+package com.amazon.app.farmerreg.view.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.amazon.app.farmerreg.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AllFarmersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_farmers, container, false)
+    }
+
+
+}
