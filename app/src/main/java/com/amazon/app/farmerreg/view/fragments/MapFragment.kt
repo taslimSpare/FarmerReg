43ef512a515206dd876a61f67f355e0e8bd932ea
Taslim Oseni <tasleemoseni@gmail.com>
2020-05-27 13:35:14 +0100

    Breakpoint

--- app/src/main/java/com/amazon/app/farmerreg/view/fragments/MapFragment.kt
+++ app/src/main/java/com/amazon/app/farmerreg/view/fragments/MapFragment.kt
@@ -11,14 +11,14 @@ import com.amazon.app.farmerreg.R
 /**
  * A simple [Fragment] subclass.
  */
-class CoordinatesFragment : Fragment() {
+class MapFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coordinates, container, false)
+        return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
 
